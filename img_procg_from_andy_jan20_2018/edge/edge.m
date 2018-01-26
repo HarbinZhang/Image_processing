8d5668b6eb6b3761ab334ccf500dd41e0b497a5d
Haibin Zhang <haibinzhang@Haibins-MacBook-Pro.local>
2018-01-26 13:01:48 -0500

    clown.png added

--- img_procg_from_andy_jan20_2018/edge/edge.m
+++ img_procg_from_andy_jan20_2018/edge/edge.m
@@ -1,6 +1,11 @@
 %This reads in a grayscale image and performs Sobel edge detection with threshold T.
-clear;X=imread('clown.jpg');X=double(X);H=[1 2 1;0 0 0;-1 -2 -1];
-X=(X-min(min(X)))/(max(max(X))-min(min(X)));%Scale image to range 0<X<1.
+clear;
+X=imread('clown.jpg');
+X=double(X);
+H=[1 2 1;0 0 0;-1 -2 -1];
+X=(X-min(min(X)))/(max(max(X))-min(min(X)));
+
+%Scale image to range 0<X<1.
 figure,imagesc(X),colormap(gray),axis off,title('Original image')
 T=1.3;%T=threshold-need to set this by trial and error.
 Y1=conv2(X,H);Y2=conv2(X,H');Y3=sqrt(Y1.*Y1+Y2.*Y2);%gradient image.
--- img_procg_from_andy_jan20_2018/lowpass/lowpass.m
+++ img_procg_from_andy_jan20_2018/lowpass/lowpass.m
@@ -1,8 +1,17 @@
 %Peforms brick-wall 2-D lowpass filtering of grayscale image
 %'clown.jpg' with cutoff wavenumber of Omega in both directions.
-clear;X=imread('clown.jpg');X=double(X);[N,M]=size(X);Omega=pi/4;%Omega=cutoff.
-KN=round(Omega*N/2/pi);KM=round(Omega*M/2/pi);FX=fft2(X);
-FX(KN:N+2-KN,:)=0;FX(:,KM:M+2-KM)=0;Y=real(ifft2(FX));
+clear;
+X=imread('clown.jpg');
+X=double(X);
+[N,M]=size(X);
+Omega=pi/4;%Omega=cutoff.
+KN=round(Omega*N/2/pi);
+KM=round(Omega*M/2/pi);
+FX=fft2(X);
+FX(KN:N+2-KN,:)=0;
+FX(:,KM:M+2-KM)=0;
+Y=real(ifft2(FX));
+
 figure,imagesc(X),colormap(gray),axis off,title('Original image')
 figure,imagesc(fftshift(log10(abs(FX)))),colormap(gray),axis off,...
 title('Log of magnitude of spectrum of filtered image')
