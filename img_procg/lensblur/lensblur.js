d200d4b9683f458504c08583d8c84356b70ef07d
Haibin Zhang <haibinzhang@Haibins-MacBook-Pro.local>
2018-02-01 14:18:22 -0500

    what

--- img_procg/lensblur/lensblur.js
+++ img_procg/lensblur/lensblur.js
@@ -1,333 +1,359 @@
-var FourierImageAnalysis = (function() {
-  /**********
-   * config */
-  var dims = [-1, -1]; // will be set later
-  var cc = 9e-3; // contrast constant
- 
-  /*********************
-   * working variables */
-  var canvases;
-  var ctxs;
-  var h;
-  var $h; // h hat
-  var h_; // h prime, the reconstructed h values
 
-  var start;
- 
-  /******************
-   * work functions */
-  function initFourierImageAnalysis() {
-    // event listeners
 
-    $('#omega_selector').bind('input',function(e){
-      var x = parseInt($('#omega_selector').val());
-      $('#omega_val').html(x+"%");
+var dims = [-1, -1];
+var cc = 9e-3;
+
+var canvases;
+var ctxs;
+var Y;
+var H;
+
+var start;
+
+var greyImage;
+
+window.onload = function(){
+	init();
+	loadImage('clown.png');
+}
+
+var init = function() {
+	canvases = [], ctxs = [];
+
+	$('#R_selector').bind('input',function(e){
+      var x = parseInt($('#R_selector').val());
+      $('#R_val').html(x);
+      start = +new Date();
+      transformAction();
+    });
+
+    $('#sigma_selector').bind('input',function(e){
+      var x = parseInt($('#sigma_selector').val());
+      $('#sigma_val').html(x);
+      start = +new Date();
+      transformAction();
+    });
+
+    $('#lambda_selector').bind('input',function(e){
+      var x = parseFloat($('#lambda_selector').val());
+      $('#lambda_val').html(x);
+      start = +new Date();
+      reconstructAction();
     });
 
     loadImage('clown.png');
  
-    $s('#transform-btn').addEventListener('click', function() {
-      start = +new Date();
- 
-      if (!h()) {
-        return alert(
-          'You need to draw an image to canvas 1 first.'
-        );
-      }
- 
-      // placed in a callback so the UI has a chance to update
-      disableButtons(transformAction);
-    });
- 
-    $s('#reconstruct-btn').addEventListener('click', function() {
-      start = +new Date();
- 
-      if (!$h()) {
-        return alert(
-          'You first need to compute the Fourier transform.'
-        );
-      }
- 
-      // placed in a callback so the UI has a chance to update
-      disableButtons(reconstructAction);
-    });
- 
-    $s('#difference-btn').addEventListener('click', function() {
-      start = +new Date();
- 
-      if (!h_()) {
-        return alert('You haven\'t reconstructed an image yet.');
-      }
- 
-      // placed in a callback so the UI has a chance to update
-      disableButtons(differenceAction);
-    });
- 
-    // initialize the working variables
-    canvases = [], ctxs = [];
-    h = $h = h_ = function() { return false; };
-  }
+    // $s('#transform-btn').addEventListener('click', function() {
+    //   start = +new Date();
+    //   // placed in a callback so the UI has a chance to update
+    //   disableButtons(transformAction);
+    // });
 
-  function loadImage(loc) {
-    start = +new Date();
+    // $s('#reconstruct-btn').addEventListener('click', function() {
+    //   start = +new Date();
  
-    // placed in a callback so the UI has a chance to update
-    disableButtons(function() {
-      // draw the initial image
-      var img = new Image();
-      img.addEventListener('load', function() {
-        // make each canvas the image's exact size
-        dims[0] = img.width;
-        dims[1] = img.height;
-        for (var ai = 0; ai < 4; ai++) {
-          canvases[ai] = $s('#canvas'+ai);
-          canvases[ai].width = dims[0];
-          canvases[ai].height = dims[1];
-          ctxs[ai] = canvases[ai].getContext('2d');
-        }
- 
-        // draw the image to the canvas
-        ctxs[0].drawImage(img, 0, 0, img.width, img.height);
- 
-        // grab the pixels
-        var imageData = ctxs[0].getImageData(
-          0, 0, dims[0], dims[1]
-        );
-        var h_es = []; // the h values
-        for (var ai = 0; ai < imageData.data.length; ai+=4) {
-          // greyscale, so you only need every 4th value
-          h_es.push(imageData.data[ai]);
-        }
- 
-        // initialize the h values
-        h = function(n, m) {
-          if (arguments.length === 0) return h_es;
- 
-          var idx = n*dims[0] + m;
-          return h_es[idx];
-        }; // make it a function so the code matches the math
- 
-        enableButtons();
- 
-        var duration = +new Date() - start;
-        console.log(
-          'It took ' + duration + 'ms to draw the image.'
-        );
-      });
-      img.crossOrigin = "anonymous";
-      img.src = loc;
-    });
-  }
-  
-  function transformAction() {
-    // compute the h hat values
-    var h_hats = [];
-    Fourier.transform(h(), h_hats);
-    h_hats = Fourier.shift(h_hats, dims);
- 
-    // get the largest magnitude
-    var maxMagnitude = 0;
-    for (var ai = 0; ai < h_hats.length; ai++) {
-      var mag = h_hats[ai].magnitude();
-      if (mag > maxMagnitude) {
-        maxMagnitude = mag;
-      }
-    }
- 
-    var x = parseInt($('#omega_selector').val());
-    var lowPassRadius = x/100 * dims[0] /2;
+    //   // placed in a callback so the UI has a chance to update
+    //   disableButtons(reconstructAction);
+    // });
+}
 
-    Fourier.filter(h_hats, dims, lowPassRadius, NaN);
- 
-    // store them in a nice function to match the math
-    $h = function(k, l) {
-      if (arguments.length === 0) return h_hats;
-  
-      var idx = k*dims[0] + l;
-      return h_hats[idx];
-    };
- 
+
+var loadImage = function(loc){
+	start = +new Date();
+
+	var img = new Image();
+	img.addEventListener('load', function(){
+		dims[0] = img.width;
+		dims[1] = img.height;
+		for( var i = 0; i < 3; i++){
+			canvases[i] = $s('#canvas' + i);
+			canvases[i].width = dims[0];
+			canvases[i].height = dims[1];
+			ctxs[i] = canvases[i].getContext('2d');
+		}
+
+		ctxs[0].drawImage(img, 0, 0, img.width, img.height);
+
+		var imageData = ctxs[0].getImageData(0, 0, dims[0], dims[1]);
+
+
+		greyImage = new GreyImage(img.width, img.height, imageData.data);
+
+		console.log(greyImage);
+
+		var h_es = [];
+		for(var i = 0; i < imageData.data.length; i+=4){
+			h_es.push(imageData.data[i]);
+		}
+
+		h = function(n, m){
+			if(arguments.length === 0) return h_es;
+
+			var idx = n * dims[0] + m;
+			return h_es[idx];
+		};
+
+		// enableButtons();
+
+		var duration = +new Date() - start; // unary add
+		console.log('It took ' + duration + 'ms to draw the image.');
+	});
+	img.crossOrigin = "anonymous";
+	img.src = loc;
+}
+
+function transformAction() {
+
+
+	var R = $('#R_selector').val();
+	var R1 = 2*R + 1;
+	var N1 = dims[1] + 2 * R;
+	var M1 = dims[0] + 2 * R;
+
+	H = new_2D_Array(R1);
+	for(var i = 0; i < R1; i++){
+		for(var j = 0; j < R1; j++){
+			if((i-R)*(i-R) + (j-R)*(j-R) <= R*R){
+				H[i][j] = 1;
+			}
+		}
+	}
+
+	console.log(H);
+
+
+	// var H2 = new_2D_Array(6);
+	// for(var i = 0; i < 6; i++){
+	// 	for(var j = 0; j < 6; j++){
+	// 		H2[i][j] = 1;
+	// 	}
+	// }
+
+	// var Y = conv2(H, H2);
+
+
+
+	Y = conv2(H, greyImage.getData());
+	// console.log(Y);
+
+	var sigma = $('#sigma_selector').val();
+	var Noise = randn(Y.length, Y[0].length, sigma);
+	console.log("Noise: ", Noise);
+	for(var j = 0; j < Y.length; j++){
+		for(var i = 0; i < Y[0].length; i++){
+			Y[j][i] += Noise[j][i];
+		}
+	}
+
+
     // draw the pixels
     var currImageData = ctxs[1].getImageData(
       0, 0, dims[0], dims[1]
     );
-    var logOfMaxMag = Math.log(cc*maxMagnitude+1);
     for (var k = 0; k < dims[1]; k++) {
       for (var l = 0; l < dims[0]; l++) {
         var idxInPixels = 4*(dims[0]*k + l);
         currImageData.data[idxInPixels+3] = 255; // full alpha
-        var color = Math.log(cc*$h(l, k).magnitude()+1);
-        color = Math.round(255*(color/logOfMaxMag));
         // RGB are the same -> gray
         for (var c = 0; c < 3; c++) { // lol c++
-          currImageData.data[idxInPixels+c] = color;
+          currImageData.data[idxInPixels+c] = Y[k][l]*2/(H.length*H.length);
         }
       }
     }
     ctxs[1].putImageData(currImageData, 0, 0);
  
-    enableButtons();
+    // enableButtons();
  
     var duration = +new Date() - start;
     console.log('It took '+duration+'ms to compute the FT.');
-  }
-  
-  function reconstructAction() {
-    // compute the h prime values
-    var h_primes = [];
-    var h_hats = $h();
-    h_hats = Fourier.unshift(h_hats, dims);
-    Fourier.invert(h_hats, h_primes);
- 
-    // store them in a nice function to match the math
-    h_ = function(n, m) {
-      if (arguments.length === 0) return h_primes;
- 
-      var idx = n*dims[0] + m;
-      return round(h_primes[idx], 2);
-    };
- 
-    // draw the pixels
-    var currImageData = ctxs[2].getImageData(
-      0, 0, dims[0], dims[1]
-    );
-    for (var n = 0; n < dims[1]; n++) {
-      for (var m = 0; m < dims[0]; m++) {
-        var idxInPixels = 4*(dims[0]*n + m);
-        currImageData.data[idxInPixels+3] = 255; // full alpha
-        for (var c = 0; c < 3; c++) { // RGB are the same, lol c++
-          currImageData.data[idxInPixels+c] = h_(n, m);
-        }
-      }
-    }
-    ctxs[2].putImageData(currImageData, 0, 0);
- 
-    enableButtons();
- 
-    var duration = +new Date() - start;
-    console.log(
-      'It took ' + duration + 'ms to reconstruct the image.'
-    );
-  }
-  
-  function differenceAction() {
-    // find the range of the errors
-    var minError = Infinity;
-    var maxError = 0;
-    for (var n = 0; n < dims[1]; n++) {
-      for (var m = 0; m < dims[0]; m++) {
-        var error = h_(n, m) - h(n, m);
-        if (error < minError) minError = error;
-        if (error > maxError) maxError = error;
-      }
-    }
- 
-    // draw the pixels
-    var currImageData = ctxs[3].getImageData(
-      0, 0, dims[0], dims[1]
-    );
-    for (var n = 0; n < dims[1]; n++) {
-      for (var m = 0; m < dims[0]; m++) {
-        var idxInPixels = 4*(dims[0]*n + m);
-        var error = h_(n, m) - h(n, m);
-        var color = getCoolColor(
-          error, [minError, maxError]
-        );
-        for (var c = 0; c < 3; c++) {
-          currImageData.data[idxInPixels+c] = color[c];
-        }
-        currImageData.data[idxInPixels+3] = 255;
-      }
-    }
-    ctxs[3].putImageData(currImageData, 0, 0);
- 
-    enableButtons();
- 
-    var duration = +new Date() - start;
-    console.log(
-      'It took ' + duration +
-      'ms to compute the difference.'
-    );
-  }
 
-  /********************
-   * helper functions */
-  function disableButtons(callback) {
-    $s('#transform-btn').disabled = true;
-    $s('#reconstruct-btn').disabled = true;
-    $s('#difference-btn').disabled = true;
-  
-    setTimeout(callback, 6); // 6ms for the UI to update
-  }
-  
-  function enableButtons() {
-    $s('#transform-btn').disabled = false;
-    $s('#reconstruct-btn').disabled = false;
-    $s('#difference-btn').disabled = false;
-  }
-  
-  // returns array of pixel colors in the image
-  function getCoolColor(n, range) {
-    if (n === range[0] && range[0] === range[1]) {
-      return getCoolColor(2*n, [n-1, 2*n+1]);
-    }
-  
-    var raw = [1.0, 1.0, 1.0]; // white
-  
-    if (n < range[0]) n = range[0];
-    if (n > range[1]) n = range[1];
-    var dn = range[1] - range[0];
-  
-    if (n < (range[0] + 0.25 * dn)) {
-      raw[0] = 0;
-      raw[1] = 4 * (n - range[0]) / dn;
-    } else if (n < (range[0] + 0.5 * dn)) {
-      raw[0] = 0;
-      raw[2] = 1 + 4 * (range[0] + 0.25 * dn - n) / dn;
-    } else if (n < (range[0] + 0.75 * dn)) {
-      raw[0] = 4 * (n - range[0] - 0.5 * dn) / dn;
-      raw[2] = 0;
-    } else {
-      raw[1] = 1 + 4 * (range[0] + 0.75 * dn - n) / dn;
-      raw[2] = 0;
-    }
-    
-    var color = [
-      tightMap(raw[0], 0, 1, 0, 255),
-      tightMap(raw[1], 0, 1, 0, 255),
-      tightMap(raw[2], 0, 1, 0, 255)
-    ];
-    return color;
-  }
-  
-  function round(n, places) {
-    var mult = Math.pow(10, places);
-    return Math.round(mult*n)/mult;
-  }
-  
-  function tightMap(n, d1, d2, r1, r2) { // enforces boundaries
-    var raw = map(n, d1, d2, r1, r2);
-    if (raw < r1) return r1;
-    else if (raw > r2) return r2;
-    else return raw;
-  }
-  
-  // given an n in [d1, d2], return a "linearly related"
-  // number in [r1, r2]
-  function map(n, d1, d2, r1, r2) {
-    var Rd = d2-d1;
-    var Rr = r2-r1;
-    return (Rr/Rd)*(n - d1) + r1;
-  }
-  
-  function $s(id) { // for convenience
-    if (id.charAt(0) !== '#') return false;
-    return document.getElementById(id.substring(1));
-  }
-  
-  return {
-    init: initFourierImageAnalysis
-  };
-})();
 
-window.addEventListener('load', FourierImageAnalysis.init);
+}
+
+
+function reconstructAction() {
+	var FFT_len = 512;
+
+
+	FFT.init(FFT_len);
+	// FFT.init(Y.length);
+	// FFT.init(8);
+	// var FYre = [], FYim = [];
+	// var FYre = new Array(Y.length * Y[0].length);
+	// var FYim = new Array(FYre.length);
+	var FYre = new Array(FFT_len*FFT_len);
+	var FYim = new Array(FFT_len*FFT_len);
+	FYre.fill(0.0);
+	FYim.fill(0.0);
+
+	var FY_idx = 0;
+	for(var y=0; y<Y.length; y++) {
+		FY_idx = y * FFT_len;
+		for(var x=0; x<Y[0].length; x++) {
+			FYre[FY_idx++] = Y[y][x];
+		}
+	}	
+
+	// console.log(FYre);
+
+
+	// var FHre = [], FHim = [];
+	var FHre = new Array(FFT_len*FFT_len);
+	var FHim = new Array(FFT_len*FFT_len);
+	FHre.fill(0);
+	FHim.fill(0);
+
+
+	// console.log(FHre);
+
+
+
+	var H_idx = 0;
+	for(var y = 0; y < H.length; y++){
+		H_idx = y * FFT_len;
+		for(var x = 0; x < H[0].length; x++){
+			FHre[H_idx++] = H[y][x];
+		}
+	} 
+
+	console.log(FHre);
+
+
+	FFT.fft2d(FYre, FYim);
+	FFT.fft2d(FHre, FHim);
+	
+
+	console.log("FYre: ", FYre);
+	console.log("FYim: ", FYim);
+	console.log("FHre: ", FHre);
+	console.log("FHim: ", FHim);
+
+
+
+	// var i = 1;
+
+	// var tempre = (FYre[i] * FHre[i] + FYim[i] * FHim[i]) / (FHre[i]*FHre[i] + FHim[i]*FHim[i] + lambda*lambda);
+	// // FXHATre.push(tempre);
+	// var tempim = (FYim[i] * FHre[i] - FYre[i] * FHim[i]) / (FHre[i]*FHre[i] + FHim[i]*FHim[i] + lambda*lambda);
+	// // FXHATim.push(tempim);
+
+	// console.log(FYre[i], FYim[i], FHre[i], FHim[i]);
+
+	// console.log(tempre, tempim);
+
+
+	var lambda = $('#lambda_selector').val();
+
+	var FXHATre = [];
+	var FXHATim = [];
+	for(var i = 0; i < FYre.length; i++){
+		var tempre = (FYre[i] * FHre[i] + FYim[i] * FHim[i]) / (FHre[i]*FHre[i] + FHim[i]*FHim[i] + lambda*lambda);
+		FXHATre.push(tempre);
+		var tempim = (FYim[i] * FHre[i] - FYre[i] * FHim[i]) / (FHre[i]*FHre[i] + FHim[i]*FHim[i] + lambda*lambda);
+		FXHATim.push(tempim);
+	}
+
+
+
+
+	// console.log(FXHATre);
+	// console.log(FXHATim);
+
+
+	// FFT.ifft2d(FYre, FYim);
+	// FFT.ifft2d(FHre, FHim);
+	FFT.ifft2d(FXHATre, FXHATim);
+	// console.log(re);
+
+
+
+	// console.log(FXHATre);
+
+	// for(var y=0; y<h; y++) {
+	// 	i = y*w;
+	// 	for(var x=0; x<w; x++) {
+	// 	  val = re[i + x];
+	// 	  p = (i << 2) + (x << 2);
+	// 	  data[p] = data[p + 1] = data[p + 2] = val;
+	// 	}
+	// }
+
+
+
+
+	// compute the h hat values
+
+
+	// var h_hats = [];
+	// Fourier.transform(h(), h_hats);
+	// h_hats = Fourier.shift(h_hats, dims);
+
+	// // get the largest magnitude
+	// var maxMagnitude = 0;
+	// for (var ai = 0; ai < h_hats.length; ai++) {
+	//   var mag = h_hats[ai].magnitude();
+	//   if (mag > maxMagnitude) {
+	//     maxMagnitude = mag;
+	//   }
+	// }
+
+	// var x = parseInt($('#omega_selector').val());
+	// var lowPassRadius = x/100 * dims[0] /2;
+
+	// Fourier.filter(h_hats, dims, lowPassRadius, NaN);
+
+	// // store them in a nice function to match the math
+	// $h = function(k, l) {
+	//   if (arguments.length === 0) return h_hats;
+
+	//   var idx = k*dims[0] + l;
+	//   return h_hats[idx];
+	// };
+
+	// // draw the pixels
+	var currImageData = ctxs[2].getImageData(
+	  0, 0, dims[0], dims[1]
+	);
+
+	// // console.log(greyImage);
+
+	for (var k = 0; k < dims[1]; k++) {
+	  for (var l = 0; l < dims[0]; l++) {
+	    var idx = (dims[0]*k + l);
+	    val = Math.round(FXHATre[FFT_len*k + l]);
+	    currImageData.data[idx*4+3] = 255; // full alpha
+	    for (var c = 0; c < 3; c++) { 
+	      currImageData.data[4*idx+c] = val;
+	    }
+	  }
+	}
+	ctxs[2].putImageData(currImageData, 0, 0);
+
+	// enableButtons();
+
+	var duration = +new Date() - start;
+	console.log('It took '+duration+'ms to reconstruct the image.');	
+}
+
+
+
+
+
+// function disableButtons(callback) {
+// 	$s('#transform-btn').disabled = true;
+// 	$s('#reconstruct-btn').disabled = true;
+
+// 	setTimeout(callback, 6); // 6ms for the UI to update
+// }
+
+
+
+// function enableButtons() {
+//   $s('#transform-btn').disabled = false;
+//   $s('#reconstruct-btn').disabled = false;
+// }
+
+function $s(id) {
+	if (id.charAt(0) !== '#') return false;
+	return document.getElementById(id.substring(1));
+}
